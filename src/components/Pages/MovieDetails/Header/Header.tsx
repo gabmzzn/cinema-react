144d2bcc423c7938b40cf1bc7b3b9afe8cee0fa2
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-07-04 04:55:28 -0300

    style

--- src/components/Pages/MovieDetails/Header/Header.tsx
+++ src/components/Pages/MovieDetails/Header/Header.tsx
@@ -13,7 +13,13 @@ export const Header = ({ title, release_date, vote_average }: HeaderProps) => {
 		<div className={css.header}>
 			<h1>{title} ({release_date.split('-')[0]})</h1>
 			<h1 style={{ display: 'flex', alignItems: 'center' }}>
-				<StarIcon sx={{ color: 'darkorange', fontSize: '1.02em' }} />
+				<StarIcon
+					sx={{
+						color: 'darkorange',
+						fontSize: '1.02em',
+						marginRight: '5px'
+					}}
+				/>
 				{vote_average === 0 ? '?' : vote_average}
 			</h1>
 		</div>
